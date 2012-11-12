688528b8d981068bc7e53d48de9f8ca47c89c167
Jon <chrono803@yahoo.com>
2012-11-12 13:26:36 -0500

    inc update to the FizzBuzz problem

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -8,7 +8,11 @@
     {
         static void Main(string[] args)
         {
-
+            Enumerable.Range(1, 100).ToList().ForEach(i => Console.WriteLine(
+                i % 5 == 0 && i % 3 == 0 ? "FizzBuzz" :
+                    i % 3 == 0 ? "Fizz" :
+                    i % 5 == 0 ? "Buzz" :
+                    i.ToString()));
         }
     }
 }
