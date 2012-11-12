48c533f8c62fff9e25183ca213ca29671a447ff9
Jon <chrono803@yahoo.com>
2012-11-12 11:17:26 -0500

    Initial commit

---
+++ DeterminePrimeNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeterminePrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            int parsedInput = 0;
+
+            if (Int32.TryParse(input, out parsedInput))
+            {
+                var items = Enumerable.Range(2, 100).Where(i => parsedInput % i == 0 && parsedInput != i);
+
+                if (items.Count() == 0)
+                {
+                    Console.WriteLine("{0} is prime", parsedInput);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Input is not a number");
+            }
+        }
+    }
+}
---
+++ SnippetsAndAlgorithms/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SnippetsAndAlgorithms
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var result = ReverseString(input);
+
+            result.ToList().ForEach(Console.Write);
+        }
+
+        private static IEnumerable<string> ReverseString(string input)
+        {
+            var resultArray = new string[input.Length];
+
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = input.Substring(i, 1);
+            }
+
+            Array.Reverse(resultArray);
+
+            return resultArray;
+        }
+    }
+}
