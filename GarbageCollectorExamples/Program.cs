4bc1cfaa7226ec61c93ceb7edb0423492487d100
Jon <chrono803@yahoo.com>
2012-11-15 15:33:50 -0500

    Added garbage collector projects for snippets to help understand how the GC works in .NET

---
+++ GarbageCollectorExamples/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GarbageCollectorExamples
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Shows that large objects are in the Large Object Heap and are automatically promoted to gen2.
+            // This helps performance since it doesn't start in gen0 and has to be promoted each time
+            // until it reaches gen2.
+            LargeObjectHeap();
+        }
+
+        private static void LargeObjectHeap()
+        {
+            var sb = new StringBuilder();
+            Console.WriteLine("StringBuilder is in gen{0}", GC.GetGeneration(sb));
+
+            var byteArray = new byte[86000];
+            Console.WriteLine("Byte array is in gen{0}", GC.GetGeneration(byteArray));
+        }
+    }
+}
