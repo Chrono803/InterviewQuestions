f9f1454e4c7c7c65afceb55bbff0b6096db5a956
Jon <jwood803@users.noreply.github.com>
2013-10-22 18:06:37 -0400

    More comments on events to be a bit more clear

--- EventsExamples/Program.cs
+++ EventsExamples/Program.cs
@@ -8,19 +8,33 @@
         {
             var logger = new Logger();
 
-            // Subscribing to the events via lambda expressions
-            logger.Logging += (s, e) => Console.WriteLine("Logging with logger type: {0}\nWith message: {1}\n\n", e.LoggerType, e.Message);
-            logger.LoggingCompleted += (s, e) => Console.WriteLine("Logging has completed");
+            // Can subscribe to the events via a method
+            logger.Logging += logger_Logging;
+            logger.LoggingCompleted += logger_LoggingCompleted;
+
+            // Can also subscribe to the events via lambda expressions
+            logger.Logging += (s, e) => Console.WriteLine("Logging with logger type: {0}\nWith message: {1}. Inside lambda.", e.LoggerType, e.Message);
+            logger.LoggingCompleted += (s, e) => Console.WriteLine("Logging has completed inside lambda.\n\n");
             
             // Making the call to the event. Each subscription is being listened and will execute
             logger.Log("Logging info message", LoggerType.Information);
 
-            // Unsubscribing to the event.
-            logger.Logging -= (s, e) => Console.WriteLine("Logging with logger type: {0}\nWith message: {1}\n\n", e.LoggerType, e.Message);
+            // Unsubscribing to the event. This won't fire as nothing is attached to it (nothing is listening)
+            logger.Logging -= (s, e) => Console.WriteLine("Logging with logger type: {0}\nWith message: {1}. Inside lambda.", e.LoggerType, e.Message);
 
             logger.Log("Unsubscribe example", LoggerType.Warning);
 
             Console.ReadLine();
+        }
+
+        static void logger_LoggingCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Logging completed inside method\n\n");
+        }
+
+        static void logger_Logging(object sender, LoggerEventArgs e)
+        {
+            Console.WriteLine("Logging with logger type: {0}\nWith message: {1}\n\n", e.LoggerType, e.Message);
         }
     }
 }
